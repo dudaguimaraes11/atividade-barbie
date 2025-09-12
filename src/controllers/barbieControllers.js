0268742f3071e3cde2ac30ffccd25ada097edd8b
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-09-12 16:07:42 -0300

    Feat: Adiciona atividade Barbie utilizando CRUD

---
+++ src/controllers/barbieControllers.js
@@ -0,0 +1,81 @@
+import dados from "../models/dados.js"; 
+const {barbies} = dados; 
+
+const getAllBarbies = (req, res) => {
+    const resultado = barbies; 
+    res.status(200).json ({
+        total: barbies.length,
+        barbies: resultado
+    })
+}
+
+const getBarbieById = (req, res) => {
+    let id = parseInt(req.params.id); 
+
+    const barbie= barbies.find(b => b.id === id);
+
+    res.status(200).json ({
+        success:true, 
+        barbie: barbie
+    })
+}
+
+const createBarbie = (req, res) => {
+    const {id, nome, profissao, anoLancamento} = req.body; 
+
+    // Se eu quiser fazer validações do tipo, casa obrigatório ou qualquer outro. 
+
+    if (!nome || !profissao) {
+        return res.status(400).json ({
+            success: false,
+            message: "Nome e profissão são obrigatórios!"
+        });
+    }
+
+    // Depois que validar, posso criar o meu bruxo usando uma const, e adicionar dentro do array.
+
+    const novaBarbie = {
+        id:  barbies.length + 1,
+        nome: nome, 
+        profissao: profissao, 
+        anoLancamento: anoLancamento
+    }
+
+    // Push no array
+    barbies.push(novaBarbie); 
+
+    res.status(201).json ({
+        success: true, 
+        message: "Barbie cadastrada com sucesso!",
+        barbie: novaBarbie
+    })
+}
+
+//NEW
+//Deletar um bruxo
+
+const deleteBarbie = (req, res) => {
+    let id = parseInt(req.params.id);
+
+    //Verificar se esse id existe!
+    const barbieParaRemover = barbies.find(b => b.id === id);
+
+    if (!barbieParaRemover) {
+        return res.status(404).json({
+            success: false,
+            message: `Essa barbie não existe, ${id}`
+        })
+    }
+
+    const barbiesFiltradas = barbies.filter(barbie => barbie.id != id);
+
+barbies.splice(0, barbies.length, ...barbiesFiltradas);
+
+res.status(200).json ({
+    success: true,
+    message: "O bruxo foi removido com sucesso",
+    barbieRemovida: barbieParaRemover
+})
+}
+
+export {getAllBarbies, getBarbieById, createBarbie, deleteBarbie};
---
+++ src/models/dados.js
@@ -0,0 +1,7 @@
+let barbies = [
+  {id: 1, nome: "Barbie Agricultora", profissao: "Agricultora", anoLancamento: 2011},
+  {id: 2, nome: "Barbie Astronauta", profissao: "Astronauta", anoLancamento: 2002},
+  {id: 3, nome: "Barbie Dev", profissao: "Desenvolvedora", anoLancamento: 2018}
+];
+
+export default {barbies}; 
